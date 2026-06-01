8e5a0a4dbcbafa246d378eaa9a3abead2bcd2d40
Joao Beleno <joao.beleno@gmail.com>
2026-06-01 13:20:35 -0300

    imagens + schedule

---
+++ config/schedule.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'itens:importar' => [
+        'frequencia' => 'weeklyOn',
+        'dia'        => 1,       // 1 = segunda-feira
+        'horario'    => '03:00',
+    ],
+
+    'receitas:importar' => [
+        'frequencia' => 'weeklyOn',
+        'dia'        => 1,
+        'horario'    => '06:00',
+    ],
+
+    'albion:importar-precos' => [
+        'frequencia' => 'everySixHours',
+    ],
+
+];
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+foreach (config('schedule') as $comando => $cfg) {
+    $freq     = $cfg['frequencia'];
+    $agendado = Schedule::command($comando);
+
+    match ($freq) {
+        'weeklyOn'       => $agendado->weeklyOn($cfg['dia'], $cfg['horario']),
+        'dailyAt'        => $agendado->dailyAt($cfg['horario']),
+        'hourlyAt'       => $agendado->hourlyAt($cfg['minuto']),
+        'everyMinutes'   => $agendado->everyMinutes($cfg['minutos']),
+        'everyHours'     => $agendado->everyHours($cfg['horas']),
+        'everySixHours'  => $agendado->everySixHours(),
+        'everyTwelveHours' => $agendado->everyTwelveHours(),
+        default          => $agendado->$freq(),
+    };
+}

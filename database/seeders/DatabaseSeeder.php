c47c00e15dc3362ba3ac1f2c0532c700bcf0a397
Joao Beleno <joao.beleno@gmail.com>
2026-06-16 11:23:33 -0300

    fix: update DatabaseSeeder to call FarmingCategoriaSeeder instead of FurnitureCategoriaSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call(QualidadeSeeder::class);
         $this->call(NivelItemSeeder::class);
         $this->call(AdminUserSeeder::class);
-        $this->call(FurnitureCategoriaSeeder::class);
+        $this->call(FarmingCategoriaSeeder::class);
         // $this->call(CategoriaSeeder::class);
     }
 }
